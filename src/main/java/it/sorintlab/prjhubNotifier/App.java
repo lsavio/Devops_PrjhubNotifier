f61db67190e8fb6f0b766fcbbe1e0c0838a560ec
Luigi Savio <luigi.savio@sorint.it>
2016-11-22 12:58:01 +0100

    prjhubnotifier-1: creazione

---
+++ src/main/java/it/sorintlab/prjhubNotifier/App.java
@@ -0,0 +1,13 @@
+package it.sorintlab.prjhubNotifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
